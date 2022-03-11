f3f3d7fe4045c3eb968b0ba20d932912dd33b3cc
ritchs <ricardohc_03@hotmail.com>
2022-03-11 13:32:43 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,205 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //card options
+  const cardArray = [
+    {
+      name: "binario",
+      img: "images/binario.png",
+    },
+    {
+      name: "rj45",
+      img: "images/rj45.png",
+    },
+    {
+      name: "bitcoin",
+      img: "images/bitcoin.png",
+    },
+    {
+      name: "camara",
+      img: "images/camara.png",
+    },
+    {
+      name: "nube",
+      img: "images/nube.png",
+    },
+    {
+      name: "pantalla",
+      img: "images/pantalla.png",
+    },
+    {
+      name: "usb",
+      img: "images/usb.png",
+    },
+    {
+      name: "cpu",
+      img: "images/cpu.png",
+    },
+    {
+      name: "ram",
+      img: "images/ram.png",
+    },
+    {
+      name: "disco",
+      img: "images/disco.png",
+    },
+    {
+      name: "mouse",
+      img: "images/mouse.png",
+    },
+    {
+      name: "teclado",
+      img: "images/teclado.png",
+    },
+    {
+      name: "rj45",
+      img: "images/rj45.png",
+    },
+    {
+      name: "binario",
+      img: "images/binario.png",
+    },
+    {
+      name: "bitcoin",
+      img: "images/bitcoin.png",
+    },
+    {
+      name: "camara",
+      img: "images/camara.png",
+    },
+    {
+      name: "nube",
+      img: "images/nube.png",
+    },
+    {
+      name: "pantalla",
+      img: "images/pantalla.png",
+    },
+    {
+      name: "usb",
+      img: "images/usb.png",
+    },
+    {
+      name: "cpu",
+      img: "images/cpu.png",
+    },
+    {
+      name: "ram",
+      img: "images/ram.png",
+    },
+    {
+      name: "disco",
+      img: "images/disco.png",
+    },
+    {
+      name: "mouse",
+      img: "images/mouse.png",
+    },
+    {
+      name: "teclado",
+      img: "images/teclado.png",
+    },
+  ];
+  cardArray.sort(() => 0.5 - Math.random());
+  let score = 0;
+  const grid = document.querySelector(".grid");
+  let resultDisplay = 0;
+  let cardsChosen = [];
+  let cardsChosenId = [];
+  let cardsWon = [];
+
+  //create your board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      const card = document.createElement("img");
+      card.setAttribute("src", "images/cubo3d.png");
+      card.setAttribute("data-id", i);
+      card.addEventListener("click", flipCard);
+      grid.appendChild(card);
+    }
+  }
+
+  //check for matches
+  function checkForMatch() {
+    const cards = document.querySelectorAll("img");
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+
+    if (optionOneId == optionTwoId) {
+      cards[optionOneId].setAttribute("src", "images/cubo3d.png");
+      cards[optionTwoId].setAttribute("src", "images/cubo3d.png");
+      UIkit.notification({
+        message: "Diste click en la misma imagen",
+        status: "warning",
+      });
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+      UIkit.notification({ message: "Encontraste el par", status: "primary" });
+      score = score + 1;
+      UIkit.notification({
+        message: "record: " + score,
+        status: "success",
+      });
+      cards[optionOneId].setAttribute("src", "images/404.png");
+      cards[optionTwoId].setAttribute("src", "images/404.png");
+      cards[optionOneId].removeEventListener("click", flipCard);
+      cards[optionTwoId].removeEventListener("click", flipCard);
+      cardsWon.push(cardsChosen);
+    } else {
+      cards[optionOneId].setAttribute("src", "images/cubo3d.png");
+      cards[optionTwoId].setAttribute("src", "images/cubo3d.png");
+      UIkit.notification({
+        message: "Lo Siento Vuelve Intentar",
+        status: "danger",
+      });
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    score = resultDisplay.toString();
+    if (cardsWon.length === cardArray.length / 2) {
+      for (let index = 0; index < 10; index++) {
+        UIkit.notification({
+          message: "---------- !G A N A S T E¡ ---------",
+          pos: "top-left",
+          status: "success",
+        });
+        UIkit.notification({
+          message: "---------- !G A N A S T E¡ ---------",
+          pos: "top-center",
+          status: "success",
+        });
+        UIkit.notification({
+          message: "---------- !G A N A S T E¡ ---------",
+          pos: "top-right",
+          status: "success",
+        });
+        UIkit.notification({
+          message: "---------- !G A N A S T E¡ ---------",
+          pos: "bottom-left",
+          status: "success",
+        });
+        UIkit.notification({
+          message: "---------- !G A N A S T E¡ ---------",
+          pos: "bottom-center",
+          status: "success",
+        });
+        UIkit.notification({
+          message: "---------- !G A N A S T E¡ ---------",
+          pos: "bottom-right",
+          status: "success",
+        });
+      }
+    }
+  }
+
+  //flip your card
+  function flipCard() {
+    let cardId = this.getAttribute("data-id");
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute("src", cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+      setTimeout(checkForMatch, 500);
+    }
+  }
+
+  createBoard();
+});
